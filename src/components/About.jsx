7a417d3fea8285f17d9d6001eaf91a9537e02416
iamsinghsudhanshu <SS7900585@GMAIL.COM>
2024-09-14 20:23:47 +0530

    Done !!

---
+++ src/components/About.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+        I have hands-on experience in web development,
+        utilizing technologies like HTML, CSS, JavaScript, 
+        ReactJs, NodeJs, and ExpressJs to build dynamic 
+        and responsive websites. My proficiency extends
+        to database management with MySQL and MongoDB, 
+        ensuring that I can handle both front-end and 
+        back-end development with ease. Additionally,
+        my problem-solving skills and ability to collaborate effectively 
+        with teams are strengths that I bring to every project.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+        I am currently pursuing a B.Sc. in Information Technology
+        from Tilak College of Science and Commerce, Navi Mumbai, 
+        where I have achieved a CGPA of 9.30. 
+        My studies have provided me with a
+        strong foundation in IT concepts and practical skills.
+        In addition to my formal education,
+        I have completed relevant training courses 
+        to enhance my expertise, including:
+        Google Cybersecurity Professional (2023)
+        Python Essentials Course (2023)
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+        I possess a diverse set of skills and expertise in various areas of IT and software development:
+
+Programming Languages: Proficient in C, C++, Java, and Python, allowing me to tackle a range of programming challenges.
+Web Development: Experienced in creating dynamic and responsive websites using HTML, CSS, JavaScript, and frameworks like ReactJs, NodeJs, and ExpressJs.
+Databases: Skilled in managing and working with databases such as MySQL and MongoDB to ensure efficient data handling and storage.
+Tools & Technologies: Knowledgeable in using AWS for cloud services and GitHub for version control, streamlining development and collaboration.
+Software: Competent in utilizing Microsoft Office and Visual Studio Code for effective project management and coding.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          [Job Title], [Company/Organization], [Dates] [Brief description of
+          responsibilities and achievements] [Job Title],
+          [Company/Organization], [Dates] [Brief description of responsibilities
+          and achievements] [Freelance/Contract Work], [Client/Organization],
+          [Dates] [Brief description of projects and contributions]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          [Award/Recognition], [Organization/Institution], [Year] [Achievement],
+          [Organization/Platform], [Year]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+        My mission is to harness my skills in IT and technology 
+        to create impactful and innovative solutions. 
+        I aim to continuously learn and adapt to new technologies, 
+        tackle challenges with creativity and problem-solving, 
+        and contribute to meaningful projects that drive success
+        and enhance user experiences.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+import pic from "../../src/sudhanshu.png";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaInstagramSquare } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
+import { ReactTyped } from "react-typed";
+
+function Home() {
+  return (
+    <>
+      <div
+        name="Home"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
+      >
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Feed</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1>Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
+              <ReactTyped
+                className="text-red-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              />
+            </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+            Hello! I’m Sudhanshu Singh, a B.Sc. IT
+            undergraduate from Tilak College of Science and Commerce, 
+            Navi Mumbai, with a passion for technology and a drive
+            to continuously learn and innovate. With a strong academic
+            foundation and a diverse set of technical skills,
+            I am eager to contribute to a dynamic IT 
+            company where I can both apply my knowledge and grow further.
+            </p>
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="https://github.com/iamsinghsudhanshu" target="_blank">
+                      <FaGithub className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/sudhanshu-singh-4200b922a?trk=contact-info" target="_blank">
+                      <FaLinkedin className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.youtube.com/@Sudhanshu__Sing" target="_blank">
+                      <IoLogoYoutube className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/sudhanshu____singh_?igsh=MTYxYnJuM2VtN3RuNQ== " target="
+                    _blank">
+                      <FaInstagramSquare className="text-2xl cursor-pointer" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" space-y-2">
+                <h1 className="font-bold text-center">Currently working on</h1>
+                <div className="flex space-x-5">
+                  <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                  <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
+            <img
+              src={pic}
+              className="rounded-full md:w-[450px] md:h-[450px] object-fit: cover;"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
+}
+
+export default Home;
